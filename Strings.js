8b06ca2e7ffd4b306d92ef1532655bd284741b9e
student <joycemumbiiwanjiku@gmail.com>
2023-03-16 05:20:28 +0300

    my strings

---
+++ Strings.js
@@ -0,0 +1,178 @@
+//  String Length()
+  //retutns the number of charaters in a string
+const one="I love strings"
+console.log(one.length)
+
+
+let String="I love strings";
+console.log(String.length);
+
+//  String replace()
+ //replace a substring
+const two=" i love strings"
+console.log(two.replace("love","understand"))
+
+
+let s="Be safe always";
+newstring = s.replace("Be safe always" ,"gooodness")
+newstring =s.replace("safe","eat")
+console.log(newstring);
+
+ //String indexOf()
+ //Returns the first index of occurrence of a value
+const three="i love python"
+console.log(three. indexOf("python"))
+const four="i love python"
+console.log(four.indexOf("love"))
+
+
+const Stri="Be kind";
+console.log(Stri.indexOf("kind"));
+
+//  String lastindexOf()
+ //Returns the last index of occurrence of a value
+
+const five="i love kotlin"
+console.log(five.lastIndexOf(" kotlin"))
+
+const myStrings="Thank you";
+console.log(myStrings.lastIndexOf("Thank"));
+//  i dont understand lastindexof
+
+
+// String startsWith()
+// Checks if a string begins with a specified string
+
+const six="Thank you"
+console.log(six.startsWith("you"))
+console.log(six.endsWith("Thank"))
+
+// String toUpperCase()
+// Returns uppercase of a string
+
+const seven="Best day"
+onsole.log(seven.toUpperCase())
+ console.log(seven.toUpperCase("day"));
+
+// string to toLowerCase
+// Returns LowerCase of a string
+
+
+ const eight="Thank you"
+console.log(eight.toLowerCase())
+
+// String includes()
+// Checks if given string is found inside a string
+
+const nine="Thank you you"
+console.log(nine.includes("you"))
+console.log(nine.includes("see"))
+console.log(nine.includes("you"))
+
+//String repeat()
+// Returns a string by repeating it given times
+
+ const ten="football is a fun game"
+console.log(ten.repeat(4));
+
+
+//  String charAt()
+// Returns character at a specified index in string
+const eleven="Thank you"
+console.log(eleven.charAt(0))
+ console.log(eleven.charAt(6))
+
+//  String charCodeAt()
+// Returns Unicode of the character at given index
+
+// const twelve="thank you"
+// console.log(twelve.charCodeAt(8))
+
+// String fromCharCode()
+//Returns a string from the given UTF-16 code units
+
+// const therteen="Thank you"
+// console.log(therteen.charCode(1))
+
+//  String substring()
+// Returns a specified part of the string
+
+//const fourteen="Thank you"
+// console.log(fourteen.substring)
+
+//  String slice()
+// Extracts and returns a section of the string
+
+// const fifteen="Thank you"
+// console.log(fifteen.substring("you"));
+
+//  String padStart()
+// Pads a string at the start to a given length
+
+// const sixteen="Thank you"
+// console.log(sixteen.padStart(2));
+
+
+//  String padEnd()
+// Pads a string at the end to a given length
+// const eighteen="Life plan"
+// console.log(eighteen.padEnd(2))
+
+
+// String codePointAt()
+// Returns the Unicode point value at given index
+
+
+// /const nneteen="life plan "
+//console.log(nneteen.codePointAt(2))
+
+//  String fromCodePoint()
+// Returns a string using the given code points
+
+// const twenty="life plan";
+// console.log(twenty.codePointAt())
+
+// Javascript String match()
+// Returns result of matching string with a regex
+
+const twentytwo="life plan"
+console.log(twentytwo.match("plan"))
+
+//  String matchAll()
+// Returns iterator of results matching with a regex
+
+// const fourty="life plan";
+// console.log(fourty.matchAll("life"));
+
+// String localeCompare()
+// Compares two strings in the current locale
+
+const  sixty="life plan is  important" 
+console.log(sixty.localeCompare("important"))
+
+
+//  String localeCompare()
+// Compares two strings in the current locale
+const seventy="life is important"
+// console.log(seventy.localeCompare("life"))
+
+// const seventyone="life is important"
+// console.log(seventyone.codePointAt("important"))//
+
+// const seventytwo="life is a blesssing"
+// console.log(seventytwo.fromCodepoint("blessing"))
+
+
+// string length
+// const seventyfive="i am learning"
+// console.log(seventyfive.length)
+// string replace
+
+// const seventysix="i am leraning"
+// console.log (seventy.replace("good things"))
+
+
+
+
+
+
